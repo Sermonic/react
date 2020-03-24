269792ad1d583a60fb124b99cf20d770a0f1686b
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-24 16:51:36 +0200

    Conditionals in JSX

--- 02_JSX/01_JSX_Basics/index.js
+++ 02_JSX/01_JSX_Basics/index.js
@@ -1,15 +1,6 @@
-function getMood() {
-  const moods = ["Angry", "Hungry", "Silly", "Quiet", "Paranoid"];
-  return moods[Math.floor(Math.random() * moods.length)];
-}
-
 class JSXDemo extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>My Current Mood is: {getMood()}</h1>
-      </div>
-    );
+    return React.createElement("h1", null, "This is Header!");
   }
 }
 
