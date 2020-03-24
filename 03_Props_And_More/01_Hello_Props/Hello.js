94525b4b9738bce162c76c2390e0d00317b55684
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-24 17:24:22 +0200

    Props are Immutable

--- 03_Props_And_More/01_Hello_Props/Hello.js
+++ 03_Props_And_More/01_Hello_Props/Hello.js
@@ -1,5 +1,9 @@
 class Hello extends React.Component {
   render() {
+    // We can't to add to it, we can't update properties that already exist in that object.
+    this.props.from = "Some name";
+    this.props.age = 10;
+
     return (
       <p>
         Hi {this.props.to} from {this.props.from}.
