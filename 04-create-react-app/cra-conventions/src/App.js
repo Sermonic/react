3023eaf44a778c00b56fbe69c3df61ea7f0192fd
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-25 12:15:26 +0200

    Create React App Conventions, CSS and Assets in CRA

---
+++ 04-create-react-app/cra-conventions/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dog from './Dog';
+
+function App() {
+  return (
+    <div className="App">
+      <Dog />
+      <div>I AM ANOTHER DIV, NOT IN DOG COMPONENT</div>
+    </div>
+  );
+}
+
+export default App;
