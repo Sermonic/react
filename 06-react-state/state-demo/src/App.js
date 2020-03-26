7d553a2ba3b239a9060f29be6dcd05670e240267
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-26 19:55:18 +0200

    Setting State Correctly

--- 06-react-state/state-demo/src/App.js
+++ 06-react-state/state-demo/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Game from "./Game";
 import Game2 from "./Game2";
+import Demo from "./Demo";
+import Rando from "./Rando";
 import "./App.css";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
       <Game />
       <Game2 />
+      <Demo animal="Bobcat" food="Pineapple" />
+      <Rando maxNum={7} />
     </div>
   );
 }
