dcd6b65f5f0c0888758a75edfeac797b1cc7b9e7
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-28 18:05:51 +0200

    dice exercise

---
+++ 07-dice-exercise/roll-dice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RollDice from "./RollDice";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ROLL DICE</h1>
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
