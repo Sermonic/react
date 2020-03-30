c512b2bf45f079549d3b81f8505d613092af83bb
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-30 14:37:31 +0300

    passing methods to child components

---
+++ 10-react-events/number-list/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NumberList from "./NumberList";
+import BetterNumberList from "./BetterNumberList";
+
+function App() {
+  return (
+    <div>
+      <NumberList />
+      <BetterNumberList />
+    </div>
+  );
+}
+
+export default App;
