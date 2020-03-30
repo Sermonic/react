43909ec877979bf5f496f75dda11eaa6236b541f
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-30 13:00:19 +0300

    binding with arguments

---
+++ 10-react-events/buttons-demo/src/ButtonList.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class ButtonList extends Component {
+  static defaultProps = {
+    colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { color: "cyan" };
+  }
+
+  changeColor(newColor) {
+    this.setState({ color: newColor });
+  }
+
+  render() {
+    return (
+      <div className="ButtonList" style={{ backgroundColor: this.state.color }}>
+        <div>
+          {this.props.colors.map(c => {
+            const colorObj = { backgroundColor: c };
+            return (
+              <button style={colorObj} onClick={this.changeColor.bind(this, c)}>
+                Click on me!
+              </button>
+            );
+          })}
+        </div>
+
+        <div>
+          {this.props.colors.map(c => {
+            const colorObj = { backgroundColor: c };
+            return (
+              <button style={colorObj} onClick={() => this.changeColor(c)}>
+                Click on me!
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ButtonList;
