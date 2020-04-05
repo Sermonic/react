a3d1e14f9f3ebd94aef9c20147e59c002798f404
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-05 13:18:31 +0300

    Box Maker Exercise

---
+++ 14-forms-exercise/box-maker-exercise/src/NewBoxForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+class NewBoxForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { height: "", width: "", color: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const newBox = { ...this.state, id: uuidv4() };
+    this.props.createBox(newBox);
+    this.setState({ height: "", width: "", color: "" });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="height">Height: </label>
+          <input
+            id="height"
+            name="height"
+            value={this.state.height}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="width">Width: </label>
+          <input
+            id="width"
+            name="width"
+            value={this.state.width}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="color">Background Color: </label>
+          <input
+            id="color"
+            name="color"
+            value={this.state.color}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button>Add New Box</button>
+      </form>
+    );
+  }
+}
+
+export default NewBoxForm;
