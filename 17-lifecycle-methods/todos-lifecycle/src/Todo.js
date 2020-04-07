d2ceefca38ce645f386edbbc11514e921428be24
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-07 19:10:59 +0300

    Introducing ComponentWillUnmount

--- 17-lifecycle-methods/todos-lifecycle/src/Todo.js
+++ 17-lifecycle-methods/todos-lifecycle/src/Todo.js
@@ -15,10 +15,14 @@ class Todo extends Component {
     this.handleToggle = this.handleToggle.bind(this);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("TODO COMPONENT DID UPDATE");
-    console.log(prevProps.task);
-    console.log(this.props.task);
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log("TODO COMPONENT DID UPDATE");
+  //   console.log(prevProps.task);
+  //   console.log(this.props.task);
+  // }
+
+  componentWillUnmount() {
+    console.log("IN COMPONENT WILL UNMOUNT");
   }
 
   handleChange(e) {
