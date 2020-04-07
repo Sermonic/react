52ef0ec19205c1f5997bc24f14f93fa8ef585b1f
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-07 17:45:44 +0300

    Adding Animated Loaders

--- 17-lifecycle-methods/lifecycle-demo/src/ZenQuote.js
+++ 17-lifecycle-methods/lifecycle-demo/src/ZenQuote.js
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./ZenQuote.css";
 
 class ZenQuote extends Component {
   constructor(props) {
     super(props);
-    this.state = { quote: "" };
+    this.state = { quote: "", isLoaded: false };
   }
 
   componentDidMount() {
     // load data
-    // set state with that data
     axios.get("https://api.github.com/zen").then((response) => {
-      this.setState({ quote: response.data });
+      setTimeout(
+        function () {
+          this.setState({ quote: response.data, isLoaded: true });
+        }.bind(this),
+        3000
+      );
     });
+    // set state with that data
   }
 
   render() {
     return (
       <div>
-        <h1>Always remember...</h1>
-        <p>{this.state.quote}</p>
+        {this.state.isLoaded ? (
+          <div>
+            <h1>Always remember...</h1>
+            <p>{this.state.quote}</p>
+          </div>
+        ) : (
+          <div className="loader-wrapper">
+            <div className="loader"></div>
+          </div>
+        )}
       </div>
     );
   }
