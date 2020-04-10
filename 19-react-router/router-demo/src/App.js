71ff62b21b783f7aeb3aa7cfb390f6304b62456f
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-10 14:05:09 +0300

    Using Switch and Exact

--- 19-react-router/router-demo/src/App.js
+++ 19-react-router/router-demo/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Dog from "./Dog";
 import "./App.css";
 
+const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/dog" component={Dog} />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/dog" component={Dog} />
+          <Route path="/dog/hater" component={Hater} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </div>
     );
   }
