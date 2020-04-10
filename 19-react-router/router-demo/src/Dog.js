77f189da18963a8664706260cfb39daf34f11231
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-10 13:29:48 +0300

    Intro to Client-Site Routing

---
+++ 19-react-router/router-demo/src/Dog.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Dog extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dog</h1>
+        <img
+          src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=360&q=80"
+          alt="Dog"
+        />
+      </div>
+    );
+  }
+}
+
+export default Dog;
