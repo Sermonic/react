8bc09956e6f37bda635d7e7b50864bb43f8e7114
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-11 20:37:27 +0300

    Intro to Vending Machine Exercise

---
+++ 19-react-router/vending-machine/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import VendingMachine from "./VendingMachine";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <VendingMachine />
+        <Chips />
+        <Sardines />
+        <Soda />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 19-react-router/vending-machine/src/Sardines.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Sardines extends Component {
+  redner() {
+    return (
+      <div>
+        <h1>Sardines</h1>
+      </div>
+    );
+  }
+}
+
+export default Sardines;
