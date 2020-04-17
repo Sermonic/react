c60935997b354b8671fa503d7ea3ec58de5a4965
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-17 18:53:00 +0300

    Multiple Route Params

--- 19-react-router/router-patterns/src/App.js
+++ 19-react-router/router-patterns/src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Food from "./Food";
+import Meal from "./Meal";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {/* This version using component works and is simpler */}
+      {/* but it will not work if you need to pass additional props */}
+      {/* <Route exact path="/food/:name" component={Food} /> */}
+
+      {/* This version using render is less clean but more explicit */}
+      {/* and you can pass in your own additional props */}
       <Route
         exact
         path="/food/:name"
         render={(routeProps) => <Food {...routeProps} />}
       />
-      <Route exact path="/food/:name" component={Food} />
+      <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
     </div>
   );
 }
