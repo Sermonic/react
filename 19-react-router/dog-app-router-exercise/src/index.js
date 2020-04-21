ecbea4dc545babf0cf442c5f9cd32279ac28cf43
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-21 16:43:41 +0300

    Writing the DogList Component

--- 19-react-router/dog-app-router-exercise/src/index.js
+++ 19-react-router/dog-app-router-exercise/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -7,9 +8,11 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
