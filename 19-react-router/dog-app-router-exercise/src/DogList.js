be5223cfd13c25799a7c3deeb50a4b8cfecef75e
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-24 12:24:15 +0300

    Styling the Dogs App

--- 19-react-router/dog-app-router-exercise/src/DogList.js
+++ 19-react-router/dog-app-router-exercise/src/DogList.js
@@ -6,14 +6,14 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1 text-center">Dog List</h1>
+        <h1 className="display-1 text-center my-4">Dog List</h1>
         <div className="row">
           {this.props.dogs.map((d) => (
             <div className="Dog col-lg-4 text-center" key={d.name}>
-              <img src={d.src} alt={d.name} />
-              <h3>
-                <Link to={`/dogs/${d.name}`}>{d.name}</Link>
-              </h3>
+              <Link to={`/dogs/${d.name}`}>
+                <img src={d.src} alt={d.name} />
+                <h3 className="underline mt-3">{d.name}</h3>
+              </Link>
             </div>
           ))}
         </div>
