3556b35a67e7648162300d7aba262f7e4e7e7a0d
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-27 16:09:23 +0300

    Displaying Shades in SingleColorPalette

--- 20-colors-app/src/App.js
+++ 20-colors-app/src/App.js
@@ -36,8 +36,15 @@ class App extends React.Component {
         />
         <Route
           exact
-          path="/palette/:palletteId/:colorId"
-          render={() => <SingleColorPalette />}
+          path="/palette/:paletteId/:colorId"
+          render={(routeProps) => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
         />
       </Switch>
     );
