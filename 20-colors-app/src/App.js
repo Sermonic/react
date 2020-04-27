a65a21aae04b362f2c8251fe65790da38c7b4919
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-27 14:45:54 +0300

    Creating SingleColorPalette

--- 20-colors-app/src/App.js
+++ 20-colors-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
@@ -36,7 +37,7 @@ class App extends React.Component {
         <Route
           exact
           path="/palette/:palletteId/:colorId"
-          render={() => <h1>Single Color Page</h1>}
+          render={() => <SingleColorPalette />}
         />
       </Switch>
     );
