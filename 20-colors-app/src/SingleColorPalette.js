5c58880b37b21a8dcb690ba30c5d6a6dda0ab2e7
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-29 13:45:41 +0300

    Move Styles Into New Folder

--- 20-colors-app/src/SingleColorPalette.js
+++ 20-colors-app/src/SingleColorPalette.js
@@ -4,48 +4,7 @@ import { withStyles } from "@material-ui/styles";
 import Navbar from "./Navbar";
 import ColorBox from "./ColorBox";
 import PaletteFooter from "./PaletteFooter";
-
-const styles = {
-  Palette: {
-    height: "90vh",
-    display: "flex",
-    flexDirection: "column",
-  },
-  colors: {
-    height: "90%",
-  },
-  goBack: {
-    width: "20%",
-    height: "50%",
-    margin: "0",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-4px",
-    opacity: 1,
-    backgroundColor: "black",
-    "& a": {
-      color: "white",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      textAlign: "center",
-      outline: "none",
-      background: "rgba(255, 255, 255, .3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      textTransform: "uppercase",
-      border: "none",
-      cursor: "pointer",
-      textDecoration: "none",
-    },
-  },
-};
+import styles from "./styles/PaletteStyles";
 
 class SingleColorPalette extends Component {
   constructor(props) {
