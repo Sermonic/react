6f2ef323b1e5d185a3b6bf5935e45eb63fb70a9a
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-29 15:10:04 +0300

    Adding NewPaletteForm

--- 20-colors-app/src/App.js
+++ 20-colors-app/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
@@ -16,6 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/"
