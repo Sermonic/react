f10d580cba6e91cd2453669beb097cad7a2cfcc5
Vitalik <vitaliyshcherbanych@gmail.com>
2020-04-30 19:25:20 +0300

    Add ColorBox Delete

--- 20-colors-app/src/DraggableColorBox.js
+++ 20-colors-app/src/DraggableColorBox.js
@@ -35,12 +35,12 @@ const styles = {
 };
 
 function DraggableColorBox(props) {
-  const { classes } = props;
+  const { name, color, classes, handleClick } = props;
   return (
-    <div className={classes.root} style={{ backgroundColor: props.color }}>
+    <div className={classes.root} style={{ backgroundColor: color }}>
       <div className={classes.boxContent}>
-        <span>{props.name}</span>
-        <DeleteIcon className={classes.deleteIcon} />
+        <span>{name}</span>
+        <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
       </div>
     </div>
   );
