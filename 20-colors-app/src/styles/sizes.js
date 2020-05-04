58caa3265f2cee5ea300064f368d7a5c2dea1435
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-04 15:31:33 +0300

    Make PaletteList responsive

--- 20-colors-app/src/styles/sizes.js
+++ 20-colors-app/src/styles/sizes.js
@@ -3,10 +3,11 @@ export default {
 
   down(size) {
     const sizes = {
-      xs: "576px",
-      sm: "768px",
-      md: "992px",
-      lg: "1200px",
+      xs: "575.98px",
+      sm: "767.98px",
+      md: "991.98px",
+      lg: "1199.98px",
+      xl: "1399.98px",
     };
     return `@media (max-width: ${sizes[size]})`;
   },
