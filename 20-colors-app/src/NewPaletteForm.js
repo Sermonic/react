de0a9ff8e7966271aa76635e5cfa3d68c1ee00e1
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-05 12:03:52 +0300

    Add basic route transitions

--- 20-colors-app/src/NewPaletteForm.js
+++ 20-colors-app/src/NewPaletteForm.js
@@ -154,7 +154,7 @@ class NewPaletteForm extends Component {
             removeColor={this.removeColor}
             axis="xy"
             onSortEnd={this.onSortEnd}
-            distance={20}
+            // distance={20}
           />
         </main>
       </div>
