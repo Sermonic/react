2cbcb25f1b1c1284e4a44ec4e6dc5ca6db9606a3
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-08 14:30:05 +0300

    The useEffect hook

--- 21-hooks/src/App.js
+++ 21-hooks/src/App.js
@@ -5,11 +5,13 @@ import Toggler from "./Toggler";
 import SimpleFormClass from "./SimpleFormClass";
 import SimpleFormHooks from "./SimpleFormHooks";
 import SimpleFormInputHook from "./SimpleFormInputHook";
+import Clicker from "./Clicker";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Clicker />
       <SimpleFormInputHook />
       <SimpleFormHooks />
       <SimpleFormClass />
