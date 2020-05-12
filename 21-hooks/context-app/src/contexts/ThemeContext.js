33568a848ddff508e158a5d67397a44ae654912f
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-12 18:42:23 +0300

    Updating a context dynamically

--- 21-hooks/context-app/src/contexts/ThemeContext.js
+++ 21-hooks/context-app/src/contexts/ThemeContext.js
@@ -5,11 +5,19 @@ export const ThemeContext = React.createContext();
 export class ThemeProvider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isDarkMode: true };
+    this.state = { isDarkMode: false };
+    this.toggleTheme = this.toggleTheme.bind(this);
   }
+
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
+  }
+
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
