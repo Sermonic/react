2a1b254b8bc5d824227cd6976ffd8227d721e884
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-13 15:00:46 +0300

    Consuming contexts enter the higher order component

--- 21-hooks/context-app/src/Navbar.js
+++ 21-hooks/context-app/src/Navbar.js
@@ -9,6 +9,22 @@ import { Switch } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import styles from "./styles/NavbarStyles";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { withLanguageContext } from "./contexts/LanguageContext";
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "🇺🇸",
+  },
+  russian: {
+    search: "Поиск",
+    flag: "🇷🇺",
+  },
+  ukrainian: {
+    search: "Пошук",
+    flag: "🇺🇦",
+  },
+};
 
 class Navbar extends Component {
   static contextType = ThemeContext;
@@ -16,13 +32,15 @@ class Navbar extends Component {
   render() {
     const { classes } = this.props;
     const { isDarkMode, toggleTheme } = this.context;
+    const { language } = this.props.languageContext;
+    const { search, flag } = content[language];
 
     return (
       <div className={classes.root}>
         <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
           <Toolbar>
             <IconButton className={classes.menuButton} color="inherit">
-              <span>🇺🇦</span>
+              <span>{flag}</span>
             </IconButton>
             <Typography className={classes.title} variant="h6" color="inherit">
               App Title
@@ -34,7 +52,7 @@ class Navbar extends Component {
                 <SearchIcon />
               </div>
               <InputBase
-                placeholder="Search..."
+                placeholder={`${search}...`}
                 classes={{
                   root: classes.inputRoot,
                   input: classes.inputInput,
@@ -48,4 +66,4 @@ class Navbar extends Component {
   }
 }
 
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
