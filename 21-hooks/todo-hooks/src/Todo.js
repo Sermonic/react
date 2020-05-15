40ac41d9248a6468e1d70ac88afec623a372ec44
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-15 14:35:36 +0300

    Optimizing with memo

--- 21-hooks/todo-hooks/src/Todo.js
+++ 21-hooks/todo-hooks/src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, memo } from "react";
 import useToggleState from "./hooks/useToggleState";
 import { DispatchContext } from "./contexts/todos.context";
 import { ListItem } from "@material-ui/core";
@@ -46,4 +46,4 @@ function Todo({ id, task, completed }) {
   );
 }
 
-export default Todo;
+export default memo(Todo);
