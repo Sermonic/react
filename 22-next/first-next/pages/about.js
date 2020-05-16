f10eef56738f6c1fa61c51af92f517b8f5318845
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-16 12:02:00 +0300

    Next's link component

--- 22-next/first-next/pages/about.js
+++ 22-next/first-next/pages/about.js
@@ -1,3 +1,6 @@
-const AboutPage = () => <h1>This Is the About Page!</h1>;
+const AboutPage = () => {
+  console.log("Running About Page!");
+  return <h1>This Is the About Page!</h1>;
+};
 
 export default AboutPage;
--- 22-next/first-next/pages/index.js
+++ 22-next/first-next/pages/index.js
@@ -1,7 +1,19 @@
-const Page = () => (
-  <div>
-    <h1>Index Page!</h1>
-  </div>
-);
+import Link from "next/link";
+
+const Page = () => {
+  console.log("Running Index Page!");
+
+  return (
+    <div>
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact Page</a>
+      </Link>
+      <h1>Index Page!</h1>
+    </div>
+  );
+};
 
 export default Page;
