6756585e152a3c1005109ac4fb3f35d1fa124aec
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-16 18:54:16 +0300

    Introducing getInitialProps

--- 22-next/first-next/pages/index.js
+++ 22-next/first-next/pages/index.js
@@ -1,9 +1,20 @@
-const Page = () => {
+const Page = ({ users }) => {
   return (
     <div>
       <h1>Index Page!</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
+};
+
+Page.getInitialProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const json = await res.json();
+  return { users: json };
 };
 
 export default Page;
