c9862aa09bca1301bde9b10cffa41a7b2b44dab5
Vitalik <vitaliyshcherbanych@gmail.com>
2020-05-19 17:21:41 +0300

    Updates

---
+++ precache-manifest.3bdc6d17c3c47e1d362c69fb72f10619.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f1b7fd61eb98ed4215c7953c3de2b5",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "e1da4670ff5f31cb3a1a",
+    "url": "/react/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "4143020bfb0a248da520",
+    "url": "/react/static/css/main.71d5ce6c.chunk.css"
+  },
+  {
+    "revision": "e1da4670ff5f31cb3a1a",
+    "url": "/react/static/js/2.cdff01b3.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react/static/js/2.cdff01b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4143020bfb0a248da520",
+    "url": "/react/static/js/main.6792595c.chunk.js"
+  },
+  {
+    "revision": "23def8c6974553eef3b7",
+    "url": "/react/static/js/runtime-main.d5b18cf8.js"
+  },
+  {
+    "revision": "1f3326703f60f4e60210a9eabeb851db",
+    "url": "/react/static/media/bg.1f332670.svg"
+  }
+]);
